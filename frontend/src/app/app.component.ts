23d625321b6ef5853e47dbfe0b362012156dac87
ThanTuan <tuan.thanhoanganh@gmail.com>
2025-05-02 21:30:23 +0700

    merge code of bao

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,6 @@
 import { TuiRoot } from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {TuiButton} from '@taiga-ui/core';
-import { Accommodation, CreateAccommodation, UpdateAccommodation } from "./models/accommodation.model";
-import { FormControl, FormGroup } from "@angular/forms";
-import { AccommodationService } from "./services/accommodation.service";
-import { HomePageComponent } from "./views/home-page/home-page.component";
-
 @Component({
     selector: 'app-root',
     imports: [RouterOutlet, TuiRoot],
