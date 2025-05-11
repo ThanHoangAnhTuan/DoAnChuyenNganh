96b3a2c8a6ed9bc06df214e5b8b4d4937f2b7c0e
Vinh <luudinhquangvinh1@gmail.com>
2025-05-11 22:18:50 +0700

    push again

--- frontend/src/app/components/search-box/search-box.component.ts
+++ frontend/src/app/components/search-box/search-box.component.ts
@@ -34,7 +34,6 @@ export default class SearchBoxComponent {
         'Ha Noi',
         'Da Nang',
         'Nha Trang',
-        'Nha Trang',
         'Hue',
         'Dong Nai',
         'Vung Tau',
@@ -46,8 +45,8 @@ export default class SearchBoxComponent {
         'Binh Thuan',
         'Cao Bang',
     ]
-    protected readonly control = new FormControl();
-    protected searchCity = new FormControl('', Validators.required);
+    protected readonly DayControl = new FormControl();
+    protected searchCityControl = new FormControl('', Validators.required);
 
     protected readonly today = TuiDay.currentLocal(); // Lấy ngày hiện tại
 
@@ -58,7 +57,7 @@ export default class SearchBoxComponent {
             this.city = params['city'];
         });
         //thiết lập validator cho date-range
-        this.control.setValidators([
+        this.DayControl.setValidators([
             control => {
                 const value = control.value;
                 if (!value) return null;
@@ -80,7 +79,7 @@ export default class SearchBoxComponent {
      * @returns true nếu input không hợp lệ và đã được touch
      */
     get isSearchCityInvalid() {
-        return this.searchCity.invalid && this.searchCity.touched;
+        return this.searchCityControl.invalid && this.searchCityControl.touched;
     }
 
     /**
@@ -90,16 +89,16 @@ export default class SearchBoxComponent {
      * - Nếu không có date range: chỉ chuyển hướng với thành phố
      */
     search() {
-        if (this.searchCity.invalid) {
-            this.searchCity.markAllAsTouched(); //Đánh dấu touched để hiển thị lỗi
+        if (this.searchCityControl.invalid) {
+            this.searchCityControl.markAllAsTouched(); //Đánh dấu touched để hiển thị lỗi
             return;
         }
-        if (this.control.value) {
+        if (this.DayControl.value) {
             // Định dạng ngày check-in và check-out
-            const checkIn = `${this.control.value?.from.formattedDayPart}-${this.control.value?.from.formattedMonthPart}-${this.control.value?.from.formattedYear}`;
-            const checkOut = `${this.control.value?.to.formattedDayPart}-${this.control.value?.to.formattedMonthPart}-${this.control.value?.to.formattedYear}`;
+            const checkIn = `${this.DayControl.value?.from.formattedDayPart}-${this.DayControl.value?.from.formattedMonthPart}-${this.DayControl.value?.from.formattedYear}`;
+            const checkOut = `${this.DayControl.value?.to.formattedDayPart}-${this.DayControl.value?.to.formattedMonthPart}-${this.DayControl.value?.to.formattedYear}`;
             //Chuyển hướng với thành phố và ngày người dùng đã nhập
-            this.router.navigate(['/search', this.searchCity.value], {
+            this.router.navigate(['/search', this.searchCityControl.value], {
                 queryParams: {
                     checkIn,
                     checkOut
@@ -108,7 +107,7 @@ export default class SearchBoxComponent {
             return;
         }
         //chuyển hướng chỉ với thành phố
-        this.router.navigate(['/search', this.searchCity.value]);
+        this.router.navigate(['/search', this.searchCityControl.value]);
         return;
     }
 }
