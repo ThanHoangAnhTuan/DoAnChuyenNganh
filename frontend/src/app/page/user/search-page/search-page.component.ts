a051da07034b926053c8307938dfa6defd2f0737
Vinh <luudinhquangvinh1@gmail.com>
2025-05-25 22:49:58 +0700

    pull code from Tuan

--- frontend/src/app/page/user/search-page/search-page.component.ts
+++ frontend/src/app/page/user/search-page/search-page.component.ts
@@ -10,7 +10,7 @@ import {
     TuiTextfieldControllerModule,
 } from '@taiga-ui/legacy';
 import { HotelService } from '../../../services/user/hotel.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-search-page',
@@ -24,6 +24,7 @@ import { ActivatedRoute } from '@angular/router';
         TuiTextfieldControllerModule,
         TuiIcon,
         NgIf,
+        RouterLink,
     ],
     templateUrl: './search-page.component.html',
     standalone: true,
@@ -123,9 +124,10 @@ export class SearchPageComponent implements OnInit {
      * Tải danh sách khách sạn từ service
      */
     loadHotels(): void {
-        this.hotelService.getHotelDetailByCity(this.city).subscribe({
+        this.hotelService.getAccommodationsByCity(this.city).subscribe({
             next: (hotels) => {
                 this.hotels = hotels.data;
+                console.log('Hotels loaded:', this.hotels);
                 // Lọc khách sạn theo thành phố từ URL
                 if (this.city && this.city.trim() !== '') {
                     this.filteredHotels = this.hotels.filter((hotel) =>
