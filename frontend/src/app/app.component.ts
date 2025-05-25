64d3c913d828475219e7f4ba57b9ce97eb731685
Vinh <luudinhquangvinh1@gmail.com>
2025-05-25 21:52:25 +0700

    fix port 3307 to 3306

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, TuiRoot, TuiRoot],
+    imports: [RouterOutlet, TuiRoot],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
