07607a17d5b892d025137fc4dcdbc79e23694334
BaoNVQ2903 <nguyenvoquocbao292003@gmail.com>
2025-06-05 21:45:41 +0700

    review in accommodation detail

--- frontend/src/app/page/user/accommodation-detail/accommodation-detail.component.ts
+++ frontend/src/app/page/user/accommodation-detail/accommodation-detail.component.ts
@@ -1,17 +1,34 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { AccommodationDetailService } from '../../../services/user/accommodation-detail.service';
 import { ActivatedRoute } from '@angular/router';
-import { NgClass, NgFor, NgIf } from '@angular/common';
+import { CommonModule, DatePipe, NgClass, NgFor, NgIf } from '@angular/common';
 import { TuiLike } from '@taiga-ui/kit';
 import { ImageListModalComponent } from '../../../components/modals/image-list-modal/image-list-modal.component';
 import { NavbarComponent } from "../../../components/navbar/navbar.component";
 import SearchBoxComponent from "../../../components/search-box/search-box.component";
 import { RoomService } from '../../../services/user/room.service';
 import { RoomInformationModalComponent } from "../../../components/modals/room-information-modal/room-information-modal.component";
+import { ReviewService } from '../../../services/user/review.service';
+import { GetAccommodationByIdResponse } from '../../../models/manager/accommodation.model';
+import { GetAccommodationDetailsResponse } from '../../../models/manager/accommodation-detail.model';
+import { Review } from '../../../models/user/review.model';
+import { ReviewListModalComponent } from "../../../components/modals/review-list-modal/review-list-modal.component";
 
 @Component({
   selector: 'app-accommodation-detail',
-  imports: [NgIf, NgFor, NgClass, TuiLike, ImageListModalComponent, NavbarComponent, SearchBoxComponent, RoomInformationModalComponent],
+  imports: [
+    NgIf,
+    NgFor,
+    NgClass,
+    TuiLike,
+    ImageListModalComponent,
+    NavbarComponent,
+    SearchBoxComponent,
+    RoomInformationModalComponent,
+    CommonModule,
+    ReviewListModalComponent
+  ],
+  providers: [DatePipe],
   templateUrl: './accommodation-detail.component.html',
   styleUrl: './accommodation-detail.component.scss'
 })
@@ -19,9 +36,13 @@ export class AccommodationDetailComponent implements OnInit {
   accommodationId: string = '';
   accommodation: any;
   rooms: any[] = [];
+  reviews: any[] = [];
   isModalOpen: boolean = false;
   isRoomInformationModalOpen: boolean = false;
+  isReviewModalOpen: boolean = false;
+  isReviewListModalOpen: boolean = false;
   roomInformationSelected: any = null;
+  reviewSelected: any = null;
   windowWidth: number = 0;
   showFull: boolean = false;
   isMobile: boolean = false;
@@ -56,15 +77,18 @@ export class AccommodationDetailComponent implements OnInit {
   selectedRooms: {
     [roomId: number]: { quantity: number; total: number };
   } = {};
+  avarageRating: number = 0;
+  scrollY: number = 0;
 
   constructor(
     private accommodationDetailService: AccommodationDetailService,
     private route: ActivatedRoute,
-    private roomService: RoomService
+    private roomService: RoomService,
+    private reviewService: ReviewService,
+    private datePipe: DatePipe,
   ) {
     this.windowWidth = window.innerWidth; // Gán giá trị của windowWidth bằng với width của màn hình
     this.updateDescription();
-    console.log(this.isRoomInformationModalOpen);
   }
 
   // Lắng nghe sự kiện mỗi khi thay đổi kích thước màn hình
@@ -80,24 +104,45 @@ export class AccommodationDetailComponent implements OnInit {
     if (this.accommodationId) {
       this.getAccommodationById(this.accommodationId);
       this.getRoomByAccommodationId(this.accommodationId);
-      // this.getHotelByName(this.accommodationName);
+      this.getReviewByAccommodationId(this.accommodationId);
     } else {
       console.error('Accommodation name is missing in URL');
     }
   }
 
   getAccommodationById(id: string) {
-    this.accommodationDetailService.getAccommodationDetailById(id).subscribe((data: any) => {
+    this.accommodationDetailService.getAccommodationDetailById(id).subscribe((data: GetAccommodationByIdResponse) => {
       this.accommodation = data.data;
-      console.log("accommodation: ", this.accommodation);
+      // console.log("accommodation: ", this.accommodation);
     })
   }
 
   getRoomByAccommodationId(id: string) {
-    this.roomService.getRoomDetailByAccommodationId(id).subscribe((data: any) => {
+    this.roomService.getRoomDetailByAccommodationId(id).subscribe((data: GetAccommodationDetailsResponse) => {
       this.rooms = data.data;
-      console.log("room: ", this.rooms);
+      // console.log("room: ", this.rooms);
     })
+  }
+
+  getReviewByAccommodationId(id: string) {
+    this.reviewService.getReviewsByAccommodationId(id).subscribe((data: Review[]) => {
+      if (data && data.length > 0) {
+        const sortedReviews = data.sort((a, b) => {
+          return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+        });
+
+        this.reviews = sortedReviews;
+
+        const totalRating = this.reviews.reduce((sum: number, review: any) => sum + review.rating, 0);
+        this.avarageRating = Math.floor((totalRating / this.reviews.length) * 10) / 10;
+
+        // console.log("reviews: ", this.reviews);
+      } else {
+        this.reviews = [];
+        this.avarageRating = 0;
+        console.log("No reviews found for this accommodation.");
+      }
+    });
   }
 
   goToLink(url: string) {
@@ -159,5 +204,58 @@ export class AccommodationDetailComponent implements OnInit {
     this.roomInformationSelected = null;
     this.isRoomInformationModalOpen = false;
     document.body.style.overflow = 'auto';
+  }
+
+  formatDate(dateStr: string): string {
+    const date = new Date(dateStr);
+    const hoursMinutes = this.datePipe.transform(date, 'HH:mm');
+    const day = date.getUTCDate();
+    const month = date.getUTCMonth() + 1;
+    const year = date.getUTCFullYear();
+    return `${hoursMinutes} ngày ${day} tháng ${month} năm ${year}`;
+  }
+
+  toggleOpenReviewModal(review: any) {
+    this.reviewSelected = review;
+    this.isReviewModalOpen = true;
+    this.scrollY = window.scrollY;
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${this.scrollY}px`;
+    document.body.style.left = '0';
+    document.body.style.right = '0';
+    document.body.style.width = '100%';
+  }
+
+  toggleCloseReviewModal() {
+    this.reviewSelected = null;
+    this.isReviewModalOpen = false;
+    document.body.style.position = '';
+    document.body.style.top = '';
+    document.body.style.left = '';
+    document.body.style.right = '';
+    document.body.style.width = '';
+    // Trả lại vị trí scroll cũ
+    window.scrollTo(0, this.scrollY);
+  }
+
+  toggleOpenReviewListModal() {
+    this.isReviewListModalOpen = true;
+    this.scrollY = window.scrollY;
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${this.scrollY}px`;
+    document.body.style.left = '0';
+    document.body.style.right = '0';
+    document.body.style.width = '100%';
+  }
+
+  toggleCloseReviewListModal() {
+    this.isReviewListModalOpen = false;
+    document.body.style.position = '';
+    document.body.style.top = '';
+    document.body.style.left = '';
+    document.body.style.right = '';
+    document.body.style.width = '';
+    // Trả lại vị trí scroll cũ
+    window.scrollTo(0, this.scrollY);
   }
 }
